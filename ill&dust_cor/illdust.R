5807ba956550fbdef770b7d735c336d1f719528e
kdm0904 <45680681+kdm0904@users.noreply.github.com>
2019-01-22 16:39:10 +0900

    ill&finedust_cor(data)

---
+++ ill&dust_cor/illdust.R
@@ -0,0 +1,11 @@
+dat <- read.csv('C:/Users/USER/Desktop/프로젝트/csv/R그래프용/ill.csv')
+cor(dat)
+library(ggplot2)
+ggplot(dat, aes(J18, PM10))+ geom_point() + geom_smooth(method = "lm")
+ggplot(dat, aes(J18, PM2.5))+ geom_point() + geom_smooth(method = "lm")
+ggplot(dat, aes(J44, PM10))+ geom_point() + geom_smooth(method = "lm")
+ggplot(dat, aes(J44, PM2.5))+ geom_point() + geom_smooth(method = "lm")
+ggplot(dat, aes(J45, PM10))+ geom_point() + geom_smooth(method = "lm")
+ggplot(dat, aes(J45, PM2.5))+ geom_point() + geom_smooth(method = "lm")
+corrplot(dat)
+
